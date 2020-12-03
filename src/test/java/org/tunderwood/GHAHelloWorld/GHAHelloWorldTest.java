528ff2866257f71b16cb14219b558e0954be0618
Todd Underwood <todd.underwood@nike.com>
2020-12-03 10:20:17 -0800

    remove unused import

--- src/test/java/org/tunderwood/GHAHelloWorld/GHAHelloWorldTest.java
+++ src/test/java/org/tunderwood/GHAHelloWorld/GHAHelloWorldTest.java
@@ -3,7 +3,6 @@ package org.tunderwood.GHAHelloWorld;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
