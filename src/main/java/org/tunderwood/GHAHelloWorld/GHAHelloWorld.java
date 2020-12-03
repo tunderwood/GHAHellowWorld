69304c09b8ce0104e47a7108be012d0e06f8b57f
Todd Underwood <todd.underwood@nike.com>
2020-12-03 09:32:17 -0800

    Initial release of code

---
+++ src/main/java/org/tunderwood/GHAHelloWorld/GHAHelloWorld.java
@@ -0,0 +1,11 @@
+package org.tunderwood.GHAHelloWorld;
+
+public class GHAHelloWorld {
+    public static void main(String[] args) {
+        hello(args[0]);
+    }
+
+    public static void hello(String name) {
+        System.out.println("Hello " + name);
+    }
+}
---
+++ src/test/java/org/tunderwood/GHAHelloWorld/GHAHelloWorldTest.java
@@ -0,0 +1,32 @@
+package org.tunderwood.GHAHelloWorld;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GHAHelloWorldTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testHello() {
+        GHAHelloWorld.hello("MyName");
+        assertEquals("Hello MyName\n", outContent.toString());
+    }
+}
